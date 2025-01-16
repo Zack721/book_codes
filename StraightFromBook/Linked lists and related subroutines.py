c9456c8090769f84802dbe610c0fe2ff292cce65
Zack721 <isaactaylor411@outlook.com>
2025-01-16 10:51:32 +0800

    Has been updated

--- StraightFromBook/Linked lists and related subroutines.py
+++ StraightFromBook/Linked lists and related subroutines.py
@@ -2,79 +2,95 @@
 NULLPOINTER = -1
 
 # Declare record type to store data and pointer
-class ListNode:
-    def __init__(self):
-        self.Data = ""
-        self.Pointer = NULLPOINTER
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.pointer = NULLPOINTER    
 
-def InitialiseList():
-    List = [ListNode() for i in range(8)]
-    StartPointer = NULLPOINTER  # set start pointer
-    FreeListPtr = 0  # set starting position of free list
-    for Index in range(7):  # link all nodes to make free list
-        List[Index].Pointer = Index + 1
-    List[7].Pointer = NULLPOINTER  # last node of free list
-    return (List, StartPointer, FreeListPtr)
+class LinkedList:
+    def __init__(LL, max_capacity):
+        LL.list_ = [Node() for i in range(max_capacity)]
+        LL.__start_ = NULLPOINTER  # set start pointer
+        LL.free_list_ptr_ = 0  # set starting position of free list
+        LL.__size_ = 0
+        LL.__capacity_ = max_capacity
 
-def InsertNode(List, StartPointer, FreeListPtr, NewItem):
-    if FreeListPtr != NULLPOINTER:
-        # there is space in the array
-        # take node from free list and store data item
-        NewNodePtr = FreeListPtr
-        List[NewNodePtr].Data = NewItem
-        FreeListPtr = List[FreeListPtr].Pointer
+        for Index in range(7):  # link all nodes to make free list
+            LL.list_[Index].pointer = Index + 1
+        LL.list_[max_capacity - 1].pointer = NULLPOINTER  # last node of free list
+        #return (List, StartPointer, FreeListPtr)
 
-        # find insertion point
-        PreviousNodePtr = NULLPOINTER
+    def InsertNode(LL, new_item):#List, StartPointer, FreeListPtr, NewItem
+        if LL.free_list_ptr_ != NULLPOINTER:  #is empty?
+            current_index = LL.free_list_ptr_   
+            LL.list_[current_index].data = new_item
+            LL.free_list_ptr_ = LL.list_[LL.free_list_ptr_].pointer
+           
+            previous_node_ptr = NULLPOINTER
+            this_node_ptr = LL.__start_  # start at beginning of list
+
+            while this_node_ptr != NULLPOINTER and LL.list_[this_node_ptr].data < new_item:   #HAS SORTING MECHANISM "and LL.list_[this_node_ptr].data < new_item"
+                # while not end of list
+                previous_node_ptr = this_node_ptr  # remember this node
+                # follow the pointer to the next node
+                this_node_ptr = LL.list_[this_node_ptr].pointer
+
+            if previous_node_ptr == NULLPOINTER:
+                LL.list_[current_index].pointer = LL.__start_ 
+                LL.__start_  = current_index
+            else:
+                # insert new node between previous node and this node
+                LL.list_[current_index].pointer = LL.list_[previous_node_ptr].pointer
+                LL.list_[previous_node_ptr].pointer = current_index
+        else:
+            print("no space for more data")
+        #return (List, StartPointer, FreeListPtr)
+
+
+
+if __name__ == "__main__":
+
+    obj = LinkedList(8)
+    obj.InsertNode("c")
+    obj.InsertNode("b")
+    obj.InsertNode("a")
+    print(obj.free_list_ptr_ )
+    
+
+    for i in obj.list_:
+        print(i.data, i.pointer)
+    
+
+
+    def FindNode(LL):#List, StartPointer, DataItem
+        # returns pointer to node
+        CurrentNodePtr = StartPointer  # start at beginning of list
+        while CurrentNodePtr != NULLPOINTER and List[CurrentNodePtr].Data != DataItem:
+            # not end of list, item not found
+            # follow the pointer to the next node
+            CurrentNodePtr = List[CurrentNodePtr].Pointer
+        #return CurrentNodePtr  # returns NullPointer if item not found
+
+    def DeleteNode(LL):#List, StartPointer, FreeListPtr, DataItem
         ThisNodePtr = StartPointer  # start at beginning of list
-
-        while ThisNodePtr != NULLPOINTER and List[ThisNodePtr].Data < NewItem:
-            # while not end of list
+        while ThisNodePtr != NULLPOINTER and List[ThisNodePtr].Data != DataItem:
+            # while not end of list and item not found
             PreviousNodePtr = ThisNodePtr  # remember this node
             # follow the pointer to the next node
             ThisNodePtr = List[ThisNodePtr].Pointer
 
-        if PreviousNodePtr == NULLPOINTER:
-            # insert new node at start of list
-            List[NewNodePtr].Pointer = StartPointer
-            StartPointer = NewNodePtr
+        if ThisNodePtr != NULLPOINTER:  # node exists in list
+            if ThisNodePtr == StartPointer:  # first node to be deleted
+                StartPointer = List[StartPointer].Pointer
+            else:
+                List[PreviousNodePtr].Pointer = List[ThisNodePtr].Pointer
+            List[ThisNodePtr].Pointer = FreeListPtr
+            FreeListPtr = ThisNodePtr
         else:
-            # insert new node between previous node and this node
-            List[NewNodePtr].Pointer = List[PreviousNodePtr].Pointer
-            List[PreviousNodePtr].Pointer = NewNodePtr
-    else:
-        print("no space for more data")
-    return (List, StartPointer, FreeListPtr)
+            print("data does not exist in list")
 
-def FindNode(List, StartPointer, DataItem):
-    # returns pointer to node
-    CurrentNodePtr = StartPointer  # start at beginning of list
-    while CurrentNodePtr != NULLPOINTER and List[CurrentNodePtr].Data != DataItem:
-        # not end of list, item not found
-        # follow the pointer to the next node
-        CurrentNodePtr = List[CurrentNodePtr].Pointer
-    return CurrentNodePtr  # returns NullPointer if item not found
-
-def DeleteNode(List, StartPointer, FreeListPtr, DataItem):
-    ThisNodePtr = StartPointer  # start at beginning of list
-    while ThisNodePtr != NULLPOINTER and List[ThisNodePtr].Data != DataItem:
-        # while not end of list and item not found
-        PreviousNodePtr = ThisNodePtr  # remember this node
-        # follow the pointer to the next node
-        ThisNodePtr = List[ThisNodePtr].Pointer
-
-    if ThisNodePtr != NULLPOINTER:  # node exists in list
-        if ThisNodePtr == StartPointer:  # first node to be deleted
-            StartPointer = List[StartPointer].Pointer
-        else:
-            List[PreviousNodePtr].Pointer = List[ThisNodePtr].Pointer
-        List[ThisNodePtr].Pointer = FreeListPtr
-        FreeListPtr = ThisNodePtr
-    else:
-        print("data does not exist in list")
-
-    return (List, StartPointer, FreeListPtr)
-
+        #return (List, StartPointer, FreeListPtr)  """
+"""
 def OutputAllNodes(List, StartPointer):
     CurrentNodePtr = StartPointer  # start at beginning of list
     if StartPointer == NULLPOINTER:
@@ -117,3 +133,4 @@ while Option != "5":
     elif Option == "4":
         OutputAllNodes(List, StartPointer)
     Option = GetOption()
+"""
--- queue_final.py
+++ queue_final.py
@@ -1,24 +1,51 @@
 class Queue:
     def __init__(self, max_capacity):
-        self.queue_ = [None for i in range(max_capacity)]
-        self.front_ = -1
-        self.end_ = -1
-        self.size_ = 0
-        self.max_ = max_capacity
+        self.__queue_ = [None for i in range(max_capacity)]
+        self.__front_ = -1
+        self.__end_ = -1
+        self.__size_ = 0
+        self.__CAPACITY_ = max_capacity
 
     def AddToQueue(self, new_item):
-        if self.size_ < self.max_:
-            if self.size_ == 0:
-                self.front_ = 0
-                self.end_ = 0
+        if self.__size_ < self.__CAPACITY_:
+            if self.__size_ == 0:
+                self.__front_ = 0
 
-            #remember to edit for wrap around
-            self.end_ += 1
-            self.queue_[self.end_] = new_item
+            self.__end_ += 1
 
-        print("Queue is at max capacity, cannot take more elements")
+            if self.__end_ > self.__CAPACITY_:
+                self.__end_ = 0
+
+            self.__queue_[self.__end_] = new_item
+            self.__size_ += 1
+
+        else:
+            print("Queue is at max capacity, cannot take more elements")
 
 
     def RemoveItem(self):
-        if  self.
+        if  self.__size_ == 0:
+            print("Queue is already empty.")
+
+        else:
+            self.__queue_[self.__front_] = None
+            self.__front_ += 1
+            self.__size_ -=1
             
+
+
+    def PrintAllInQueue(self):
+        for i in self.__queue_:
+            print(i)
+            
+        
+
+def Print():
+    print(queue.__queue_)
+queue = Queue(8)
+queue.AddToQueue(12)
+Print()
+
+queue.RemoveItem()
+Print()
+queue.PrintAllInQueue()
