11661aeda1f958934fecc79c7d569281a3343ca0
Zack721 <isaactaylor411@outlook.com>
2025-04-01 15:54:15 +0800

    Has been updated

--- StraightFromBook/binary_tree_made_in_class.py
+++ StraightFromBook/binary_tree_made_in_class.py
@@ -8,7 +8,7 @@ class Node:
 
 class BinaryTree:
     def __init__(self, capacity):
-        self.tree = [Node() for i in range(capacity)]
+        self.__tree_ = [Node() for i in range(capacity)]
         self.__root_ptr_ = NULLPOINTER
           #points to empty space in list not binary tree
         self.__size_ = 0
@@ -20,7 +20,7 @@ class BinaryTree:
     def Insert(self, new_item):
         if self.__size_ != self.__max_:
            
-            self.tree[self.__size_].data = new_item 
+            self.__tree_[self.__size_].data = new_item 
             index_of_new_item = self.__size_
 
             if self.__root_ptr_ == NULLPOINTER:  
@@ -32,7 +32,7 @@ class BinaryTree:
             current_ptr = self.__root_ptr_
             previous_item = None#if is none is initial value, when entering second value will have the followingg syntax errror: "'NoneType' object has no attribute 'left_ptr'"   #need this i think in order to assign the pointer to the new_item properly
             while  current_ptr != NULLPOINTER:
-                current_item = self.tree[current_ptr]
+                current_item = self.__tree_[current_ptr]
                 if new_item < current_item.data:
                     previous_item = current_item
                     current_ptr = current_item.left_ptr
@@ -59,14 +59,14 @@ class BinaryTree:
 
     def Find(self, item_to_find):
         current_ptr = self.__root_ptr_
-        found = False
+        #found = False
 
         while current_ptr != NULLPOINTER: #and found == False: was ginna use this but dont need it
             #print("Iterated")
-            current_item = self.tree[current_ptr]
+            current_item = self.__tree_[current_ptr]
             if current_item.data == item_to_find:
-                found = True
-                return "The node was found in the binary tree"
+                #found = True
+                return current_item
             
             elif item_to_find > current_item.data:
                 current_ptr = current_item.right_ptr
@@ -74,43 +74,31 @@ class BinaryTree:
             elif item_to_find < current_item.data:
                 current_ptr = current_item.left_ptr
 
-        return "The item is not in the binary tree"
+        return False
     
-    def Print(self):#from chatgpt
-        def traverse(index):
-            if index == NULLPOINTER: # no node at this part of binary tree so return
-                return
-            node = self.tree[index]    #node to check
-            if node.left_ptr == NULLPOINTER and node.right_ptr == NULLPOINTER:  #if it is the last leaf on the branch
-                print(node.data, end=' ') #print node
-                return
-            return traverse(node.left_ptr)        #recursive traverse through left node
-            return traverse(node.right_ptr)       #recursive traverse through right node
-        
-        if self.__root_ptr_ != NULLPOINTER: #check if you can traverse the tree
-            return traverse(self.__root_ptr_)    #treverse the tree
-        else:
-            print("The tree is empty")
-    
-            
+    def __Traverse(self, current):
+        if current != NULLPOINTER :
+            self.__Traverse(self.__tree_[current].left_ptr)
+            print(self.__tree_[current].data)
+            self.__Traverse(self.__tree_[current].right_ptr)
+                        
 
-    def ReturnSize(self):
-        return self.__size_
     
-    
+    def Print(self):
+        self.__Traverse(self.__root_ptr_)
 
 if __name__ == "__main__":
     bt = BinaryTree(8)
     bt.Insert(9)
     bt.Insert(9)
     bt.Insert(7)
-    bt.Insert(6)
-    bt.Insert(5)
-    bt.Insert(4)
-    bt.Insert(3)
-    bt.Insert(2)
+    bt.Insert(111)
+    bt.Insert(52)
+    bt.Insert(43)
+    bt.Insert(213)
+    bt.Insert(72)
    
-    find_node = bt.Find(6)
+    find_node = bt.Find(111)
     print(find_node)
 
     bt.Print() #doesnt work properly, dkw
