20a58ad9d95d277ef6bb59312ebcca0ef678be8a
Zack721 <isaactaylor411@outlook.com>
2025-05-06 11:27:34 +0800

    Has been updated

--- FileHandler/MyRouterGraph.py
+++ FileHandler/MyRouterGraph.py
@@ -1,51 +1,85 @@
 import math
 
+SPEED = 70
 
 class Station:
-    def __init__(self, id,coords):
-        self.id = id     
-        self.coord = coords  # North then west is tuple
-        self.trains_passed_by = None # is set
-        self.neighbour_st = None #is dictionary
+    def __init__(self, station_id,coords):
+        self.station_id = station_id     
+        self.coords = coords  # west then north (x, y) is tuple
+        self.trains_passed_by = set() # is set
+        self.neighbour_st = dict() #is dictionary
 
 class Train:
-    def __init__(self, id, route, speed):
-        self.id = id
+    def __init__(self, train_id, route):
+        self.train_id = train_id
         self.route = route #is list
-        self.speed = speed 
+        self.speed = SPEED 
 
 def GetTrainsPassingBy(station, train_list):   #station here is an object of the class "Station"
     for train in train_list:
         for st in train.route:
-            if station.id == st.id:
-                station.trains_passed_by = st
+            if station.station_id == st.station_id:
+                station.trains_passed_by.add(train)
                 break
 
-def DistanceCalc(station1, station2):
-    first_x = station1.coords[0]  #is tuple, dont let the [] confuse u my G 
-    first_y =
+def DistanceCalc(train_list): #doing my own thing, calcs distance between the current and next station in route
+    for train in train_list:
+        for i in range(len(train.route)-1):
+            first_x = train.route[i].coords[0] #is tuple, dont let the [] confuse u my G 
+            first_y = train.route[i].coords[1]
 
-       
+            second_x = train.route[i+1].coords[0]
+            second_y = train.route[i+1].coords[1]
+
+            #lines 35 to 49
+            # Convert degrees to radians
+            first_x = math.radians(first_x)
+            first_y = math.radians(first_y)
+            second_x = math.radians(second_x)
+            second_y = math.radians(second_y)
+
+            # Differences
+            dx = second_x - first_x
+            dy = second_y - first_y
+
+            #Haversine formula
+            a = math.sin(dy / 2)**2 + math.cos(first_y) * math.cos(second_y) * math.sin(dx / 2)**2
+            c = 2 * math.asin(math.sqrt(a))
+            r = 6371  # Earth radius in kilometers
+            distance_km = c * r
+
+            train.route[i].neighbour_st[f"distance from {train.route[i].station_id} to {train.route[i+1].station_id}"] = distance_km
+            
 
 if __name__ == "__main__":
     stations_list = []
-    train_list = [] #no diddy
+    train_list = [] #no diddy lol
 
-    stations_list.append(Station("cannon street station",(51.5114, 0.0903)))
-    stations_list.append(Station("St. James Park station",(51.4995, 0.1337)))
-    stations_list.append(Station("water loo station",(51.5032, 0.1134)))
-    stations_list.append(Station("London Bridge station ",(51.5043, 0.0848)))
-    stations_list.append(Station("wimbledon park tube station",(51.4344, 0.1994)))
-    stations_list.append(Station("London King's Cross Station", (51.5308, 0.1238)))
-    stations_list.append(Station("London Paddington Station", (51.5152, 0.1750)))
-    stations_list.append(Station("London Liverpool Street Station", (51.5178, 0.0824)))
-    stations_list.append(Station("London Victoria Station", (51.4965, 0.1439)))
-    stations_list.append(Station("Euston Station", (51.5281, 0.1337)))
+    #STation creation here and append to stations_list with arguments:stattion_id, coordinates
+    stations_list.append(Station("cannon street station",( 0.0903, 51.5114)))
+    stations_list.append(Station("St. James Park station",( 0.1337, 51.4995)))
+    stations_list.append(Station("water loo station",( 0.1134, 51.5032)))
+    stations_list.append(Station("London Bridge station ",( 0.0848, 51.5043)))
+    stations_list.append(Station("wimbledon park tube station",(0.1994, 51.4344 )))
+    stations_list.append(Station("London King's Cross Station", ( 0.1238, 51.5308)))
+    stations_list.append(Station("London Paddington Station", ( 0.1750, 51.5152)))
+    stations_list.append(Station("London Liverpool Street Station", ( 0.0824, 51.5178)))
+    stations_list.append(Station("London Victoria Station", ( 0.1439, 51.4965)))
+    stations_list.append(Station("Euston Station", (0.1337, 51.5281)))
+    
+    
+    #Train creation here and append to train_list with arguments:train_id, route(list of stations it'll pass)
+    train_list.append(Train("Goku", [stations_list[1], stations_list[4],stations_list[6], stations_list[8], stations_list[9]]))#get the stations objects straight from the list of stations
+    train_list.append(Train("Luffy", [stations_list[0], stations_list[2], stations_list[3], stations_list[5], stations_list[7]]))
+    train_list.append(Train("Flash", [stations_list[1], stations_list[3], stations_list[4], stations_list[7], stations_list[8]]))
 
-    train_list.append(Train("Goku", [stations_list[1], stations_list[4],stations_list[6], stations_list[8], stations_list[9]]), 70)
-    train_list.append(Train("Luffy", [stations_list[0], stations_list[2], stations_list[3], stations_list[5], stations_list[7]]), 70)
-    train_list.append(Train("Flash", [stations_list[1], stations_list[3], stations_list[4], stations_list[7], stations_list[8]]), 70)
 
+    for station in stations_list:    #finds trains passing by for all stations
+        GetTrainsPassingBy(station, train_list)
 
+    DistanceCalc(train_list)
+
+    for station in stations_list:
+        print(station.neighbour_st)
     
     
